ad120606603341b7c2ede0cb34f339ef5ed73669
AyushR1 <ayushrathore12501@gmail.com>
2023-01-29 11:33:49 +0530

    d
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

---
+++ frontend/build/precache-manifest.dd20a1af239735c6f34b0e064c699440.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63211fe5a374c9701c6babd4d8a537e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e615bc159da87667476",
+    "url": "/static/css/2.190caba9.chunk.css"
+  },
+  {
+    "revision": "7271adb407fb4a86a65e",
+    "url": "/static/css/main.4ce49dc1.chunk.css"
+  },
+  {
+    "revision": "8e615bc159da87667476",
+    "url": "/static/js/2.f51c74a8.chunk.js"
+  },
+  {
+    "revision": "965819429545b45732bd4c4258c140b3",
+    "url": "/static/js/2.f51c74a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7271adb407fb4a86a65e",
+    "url": "/static/js/main.86d6d7a0.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "09ecf74cbb4664b9031c5cc1e32c16ce",
+    "url": "/static/media/StarWars.09ecf74c.mp3"
+  },
+  {
+    "revision": "85f8745d712d011a336d3f3993932bcd",
+    "url": "/static/media/camera-stop.85f8745d.svg"
+  },
+  {
+    "revision": "1b1f0dc8daca05d7bb230891e4acb66b",
+    "url": "/static/media/hang-up.1b1f0dc8.svg"
+  },
+  {
+    "revision": "09d0b6c27ab9d2a540058ad888032b50",
+    "url": "/static/media/microphone-stop.09d0b6c2.svg"
+  },
+  {
+    "revision": "5f489b70a9f56150b20f126530828a5d",
+    "url": "/static/media/microphone.5f489b70.png"
+  }
+]);
